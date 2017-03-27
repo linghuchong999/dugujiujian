beecd3bc01d4efa86c5520baf317582ba4e39ddc
linghuchong <linghuchong999>
2017-03-27 20:55:03 +0800

    update 04

--- src/com/atguigu/jianfa/Dugujiujian.java
+++ src/com/atguigu/jianfa/Dugujiujian.java
@@ -4,6 +4,10 @@ public class Dugujiujian {
 
 	public static void main(String[] args) {
 		System.out.println("独孤九剑，九步九剑！");
+		
+		System.out.println("独孤九剑，第一剑！");
+		System.out.println("独孤九剑，第二剑！");
+		
 
 	}
 
